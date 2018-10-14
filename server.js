9f5dcc7d35ddd0ab1de8e951f7081c952c53f582
ilya777grin23 <IRuss23@yandex.ru>
2018-10-14 20:21:19 +0300

    Stable video cr-ed
    Stable version cr-ed
    
    Fix error "cannot create source by BaseAudioContext element"
    
    /api/events? cr-ed
    
    Fix routes
    
    Add method POST
    
    README upd
    
    $(git log --format=%B --reverse HEAD..HEAD@{1})
    
    Merge branch 'multimedia' of https://github.com/ilya777grin23/yandex-shri-task-responsive-design into multimedia
    
    * 'multimedia' of https://github.com/ilya777grin23/yandex-shri-task-responsive-design:
      README upd
      Add method POST
      Fix routes
      /api/events? cr-ed
    
    Add files
    
    Add files
    
    Merge multimedia with master
    
    Merge branch 'multimedia'
    
    * multimedia:
      Add files
      Add files
      $(git log --format=%B --reverse HEAD..HEAD@{1})
      README upd
      Add method POST
      Fix routes
      /api/events? cr-ed
      Fix error "cannot create source by BaseAudioContext element"
      Stable version cr-ed
      Stable video cr-ed
    
    Fix merge
    
    Add comments

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
-const nodeStatic = require('node-static');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-const file = new nodeStatic.Server('./dist', { cache: 0 });
-require('http').createServer((req, res) => {
-    req.addListener('end', () => {
-        file.serve(req, res);
-    }).resume();
-}).listen(3000);
+const PORT = process.env.PORT || 8000;
+
+app.set('view engine', 'pug');
+app.set('views', './src/views/components/pages');
+
+app.use(cors());
+
+app
+    .use(express.static('./dist'))
+    .use((req, res, next) => {
+        res.locals.origin = `${req.protocol}://${req.hostname}${PORT ? ':' + PORT : ''}`;
+        next();
+    })
+    .use('/status', require('./routes/status'))
+    .use('/api/events', require('./routes/api'))
+    .get('/video', (req, res) => { res.render('video'); })
+    .use('/events', require('./routes/pagination'))
+    .use((req, res, next) => {
+        const error = new Error('Ошибка :(');
+        error.status = 404;
+        next(error);
+    })
+    .use((error, req, res, next) => {
+        const title = error.status || 400;
+        const text = error.message;
+        res.render('content', { title, text });
+        next();
+    });
+    
+app.listen(PORT, err => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    console.log(`Server listening ${PORT} port`);
+});
